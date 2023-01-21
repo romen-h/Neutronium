092e049b6129c37839e7b31ca3d555d8573af13b
romenhurst <44817261+romenhurst@users.noreply.github.com>
2023-01-20 20:39:17 -0500

    WIP Audio module

---
+++ Audio/AudioManager.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+
+using FMODUnity;
+
+using UnityEngine;
+
+namespace Neutronium.Audio
+{
+	public class AudioManager
+	{
+		private static AudioManager _instance;
+
+		/// <summary>
+		/// Returns the AudioManager singleton instance.
+		/// </summary>
+		public static AudioManager Instance
+		{
+			get
+			{
+				if (_instance == null)
+				{
+					_instance = new AudioManager();
+				}
+				return _instance;
+			}
+		}
+
+		private readonly Dictionary<string, FMOD.Sound> _loadedSounds = new Dictionary<string, FMOD.Sound>();
+
+		private AudioManager()
+		{ }
+
+		/// <summary>
+		/// Loads a sound from a file.
+		/// </summary>
+		/// <param name="filePath">The path to the sound file.</param>
+		/// <param name="id">A unique string for the sound asset.</param>
+		/// <param name="positional">Toggles 3D positional audio.</param>
+		/// <param name="looping">Toggles whether the sound automatically loops.</param>
+		/// <param name="oneAtATime">Toggles whether multiple instances of the sound can be played at the same time.</param>
+		/// <returns>True if the sound was loaded.</returns>
+		public bool LoadSound(string filePath, string id, bool positional, bool looping, bool oneAtATime)
+		{
+			FMOD.MODE mode = FMOD.MODE.DEFAULT | FMOD.MODE.CREATESAMPLE;
+
+			if (positional)
+				mode |= FMOD.MODE._3D | FMOD.MODE._3D_WORLDRELATIVE;
+
+			if (looping)
+				mode |= FMOD.MODE.LOOP_NORMAL;
+
+			if (oneAtATime)
+				mode |= FMOD.MODE.UNIQUE;
+
+			FMOD.RESULT rc = RuntimeManager.CoreSystem.createSound(filePath, mode, out FMOD.Sound sound);
+
+			if (rc == FMOD.RESULT.OK)
+			{
+				_loadedSounds.Add(id, sound);
+				return true;
+			}
+			else
+			{
+				UnityEngine.Debug.LogError($"Neutronium.Audio: Failed to create sound. (id={id}, error={rc})");
+				return false;
+			}
+		}
+
+		public SoundHandle PrepareSound(string id, float volume, Vector3 position)
+		{
+			if (_loadedSounds.TryGetValue(id, out FMOD.Sound sound))
+			{
+				return PrepareSound(id, sound, volume, position);
+			}
+
+			return SoundHandle.Invalid;
+		}
+
+		/// <summary>
+		/// Starts playing the sound for the given id.
+		/// </summary>
+		/// <param name="id">The sound id.</param>
+		/// <returns>A SoundHandle instance that can be used to modify the played sound.</returns>
+		public SoundHandle PlaySound(string id)
+		{
+			if (_loadedSounds.TryGetValue(id, out FMOD.Sound sound))
+			{
+				float volume = 1f;
+				Vector3 position = SoundListenerController.Instance.transform.position;
+				return PlaySound(id, sound, volume, position);
+			}
+
+			return SoundHandle.Invalid;
+		}
+
+		/// <summary>
+		/// Starts playing the sound for the given id, with a specific volume.
+		/// </summary>
+		/// <param name="id">The sound id.</param>
+		/// <param name="volume">A value in [0,1] to scale the volume.</param>
+		/// <returns>A SoundHandle instance that can be used to modify the played sound.</returns>
+		public SoundHandle PlaySound(string id, float volume)
+		{
+			if (_loadedSounds.TryGetValue(id, out FMOD.Sound sound))
+			{
+				Vector3 position = SoundListenerController.Instance.transform.position;
+				return PlaySound(id, sound, volume, position);
+			}
+
+			return SoundHandle.Invalid;
+		}
+
+		public SoundHandle PlaySound(string id, Vector3 position)
+		{
+			if (_loadedSounds.TryGetValue(id, out FMOD.Sound sound))
+			{
+				float volume = 1f;
+				return PlaySound(id, sound, volume, position);
+			}
+
+			return SoundHandle.Invalid;
+		}
+
+		/// <summary>
+		/// Starts playing the sound for the given id, with a specific volume and 3D position.
+		/// </summary>
+		/// <param name="id">The sound id.</param>
+		/// <param name="volume">A value in [0,1] to scale the volume.</param>
+		/// <param name="position">A position in the scene for the sound to play from. (positional only)</param>
+		/// <returns>A SoundHandle instance that can be used to modify the played sound.</returns>
+		public SoundHandle PlaySound(string id, float volume, Vector3 position)
+		{
+			if (_loadedSounds.TryGetValue(id, out FMOD.Sound sound))
+			{
+				return PlaySound(id, sound, volume, position);
+			}
+
+			return SoundHandle.Invalid;
+		}
+
+		private SoundHandle PlaySound(string id, FMOD.Sound sound, float volume, Vector3 position)
+		{
+			SoundHandle handle = PrepareSound(id, sound, volume, position);
+			if (handle.Valid)
+			{
+				handle.Channel.setPaused(false);
+			}
+			return handle;
+		}
+
+		private SoundHandle PrepareSound(string id, FMOD.Sound sound, float volume, Vector3 position)
+		{
+			FMOD.RESULT r1 = RuntimeManager.CoreSystem.getMasterChannelGroup(out FMOD.ChannelGroup cg);
+			if (r1 == FMOD.RESULT.OK)
+			{
+				FMOD.RESULT r2 = RuntimeManager.CoreSystem.playSound(sound, cg, true, out FMOD.Channel channel);
+				if (r2 == FMOD.RESULT.OK)
+				{
+					FMOD.VECTOR pos = CameraController.Instance.GetVerticallyScaledPosition(position, false).ToFMODVector();
+					FMOD.VECTOR vel = new FMOD.VECTOR();
+					channel.set3DAttributes(ref pos, ref vel);
+
+					channel.setVolume(volume);
+
+					SoundHandle handle = new SoundHandle(id, channel);
+
+					return new SoundHandle(id, channel);
+				}
+			}
+
+			return SoundHandle.Invalid;
+		}
+	}
+}
---
+++ Audio/SoundHandle.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using FMODUnity;
+
+using UnityEngine;
+
+namespace Neutronium.Audio
+{
+	public class SoundHandle
+	{
+		public static readonly SoundHandle Invalid = new SoundHandle();
+
+		public bool Valid
+		{ get; internal set; }
+
+		public readonly string ID;
+		public readonly FMOD.Channel Channel;
+
+		internal SoundHandle()
+		{
+			Valid = false;
+		}
+
+		internal SoundHandle(string id, FMOD.Channel channel)
+		{
+			ID = id;
+			Channel = channel;
+			Valid = channel.handle != IntPtr.Zero;
+		}
+
+		public void Start()
+		{
+			if (Valid)
+			{
+				Channel.setPaused(false);
+			}
+		}
+
+		public void Pause()
+		{
+			if (Valid)
+			{
+				Channel.setPaused(true);
+			}
+		}
+
+		public float Volume
+		{
+			get
+			{
+				if (Valid)
+				{
+					Channel.getVolume(out float volume);
+					return volume;
+				}
+
+				return 0f;
+			}
+			set
+			{
+				if (Valid)
+				{
+					Channel.setVolume(value);
+				}
+			}
+		}
+
+		public void Destroy()
+		{
+			if (Valid)
+			{
+				Channel.stop();
+				Channel.clearHandle();
+				Valid = false;
+			}
+		}
+
+		public void SetPosition(Vector3 position)
+		{
+			FMOD.VECTOR pos = CameraController.Instance.GetVerticallyScaledPosition(position, false).ToFMODVector();
+			FMOD.VECTOR vel = new FMOD.VECTOR();
+			Channel.set3DAttributes(ref pos, ref vel);
+		}
+	}
+}
