9ca4900364e8c0b7ed1d9b32cecb2a0cbd5acd9c
romen-h <44817261+romen-h@users.noreply.github.com>
2026-08-24 17:10:49 -0400

    Constants for Klei element IDs

---
+++ NeutroniumApi.Interfaces/Elements/KleiElements/Liquids.cs
@@ -0,0 +1,167 @@
+﻿using Neutronium.Api.Meta;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Neutronium.Api.Elements.KleiElements
+{
+	[PreviewApi]
+	public static class Liquids
+	{
+		[PreviewApi]
+		public static readonly string Brine = "Brine";
+
+		[PreviewApi]
+		public static readonly string Chlorine = "Chlorine";
+
+		[PreviewApi]
+		public static readonly string CrudeOil = "CrudeOil";
+
+		[PreviewApi]
+		public static readonly string DirtyWater = "DirtyWater";
+
+		[PreviewApi]
+		public static readonly string Ethanol = "Ethanol";
+
+		[PreviewApi]
+		public static readonly string FishMilk = "FishMilk";
+
+		[PreviewApi]
+		public static readonly string Ink = "Ink";
+
+		[PreviewApi]
+		public static readonly string Latex = "Latex";
+
+		[PreviewApi]
+		public static readonly string LiquidCarbonDioxide = "LiquidCarbonDioxide";
+
+		[PreviewApi]
+		public static readonly string LiquidGunk = "LiquidGunk";
+
+		[PreviewApi]
+		public static readonly string LiquidHelium = "LiquidHelium";
+
+		[PreviewApi]
+		public static readonly string LiquidHydrogen = "LiquidHydrogen";
+
+		[PreviewApi]
+		public static readonly string LiquidMethane = "LiquidMethane";
+
+		[PreviewApi]
+		public static readonly string LiquidOxygen = "LiquidOxygen";
+
+		[PreviewApi]
+		public static readonly string LiquidPhosphorus = "LiquidPhosphorus";
+
+		[PreviewApi]
+		public static readonly string LiquidPropane = "LiquidPropane";
+
+		[PreviewApi]
+		public static readonly string LiquidSulfur = "LiquidSulfur";
+
+		[PreviewApi]
+		public static readonly string Milk = "Milk";
+
+		[PreviewApi]
+		public static readonly string Magma = "Magma";
+
+		[PreviewApi]
+		public static readonly string Mercury = "Mercury";
+
+		[PreviewApi]
+		public static readonly string MoltenAluminum = "MoltenAluminum";
+
+		[PreviewApi]
+		public static readonly string MoltenCarbon = "MoltenCarbon";
+
+		[PreviewApi]
+		public static readonly string MoltenCobalt = "MoltenCobalt";
+
+		[PreviewApi]
+		public static readonly string MoltenCopper = "MoltenCopper";
+
+		[PreviewApi]
+		public static readonly string MoltenGlass = "MoltenGlass";
+
+		[PreviewApi]
+		public static readonly string MoltenGold = "MoltenGold";
+
+		[PreviewApi]
+		public static readonly string MoltenIridium = "MoltenIridium";
+
+		[PreviewApi]
+		public static readonly string MoltenIron = "MoltenIron";
+
+		[PreviewApi]
+		public static readonly string MoltenLead = "MoltenLead";
+
+		[PreviewApi]
+		public static readonly string MoltenNickel = "MoltenNickel";
+
+		[PreviewApi]
+		public static readonly string MoltenNiobium = "MoltenNiobium";
+
+		[PreviewApi]
+		public static readonly string MoltenSalt = "MoltenSalt";
+
+		[PreviewApi]
+		public static readonly string MoltenSteel = "MoltenSteel";
+
+		[PreviewApi]
+		public static readonly string MoltenSucrose = "MoltenSucrose";
+
+		[PreviewApi]
+		public static readonly string MoltenSyngas = "MoltenSyngas";
+
+		[PreviewApi]
+		public static readonly string MoltenTungsten = "MoltenTungsten";
+
+		[PreviewApi]
+		public static readonly string MoltenUranium = "MoltenUranium";
+
+		[PreviewApi]
+		public static readonly string MoltenZinc = "MoltenZinc";
+
+		[PreviewApi]
+		public static readonly string Mucus = "Mucus";
+
+		[PreviewApi]
+		public static readonly string MurkyBrine = "MurkyBrine";
+
+		[PreviewApi]
+		public static readonly string Naphtha = "Naphtha";
+
+		[PreviewApi]
+		public static readonly string NaturalResin = "NaturalResin";
+
+		[PreviewApi]
+		public static readonly string NuclearWaste = "NuclearWaste";
+
+		[PreviewApi]
+		public static readonly string Petroleum = "Petroleum";
+
+		[PreviewApi]
+		public static readonly string PhytoOil = "PhytoOil";
+
+		[PreviewApi]
+		public static readonly string RefinedLipid = "RefinedLipid";
+
+		[PreviewApi]
+		public static readonly string Resin = "Resin";
+
+		[PreviewApi]
+		public static readonly string SaltWater = "SaltWater";
+
+		[PreviewApi]
+		public static readonly string SugarWater = "SugarWater";
+
+		[PreviewApi]
+		public static readonly string SuperCoolant = "SuperCoolant";
+
+		[PreviewApi]
+		public static readonly string ViscoGel = "ViscoGel";
+
+		[PreviewApi]
+		public static readonly string Water = "Water";
+	}
+}
---
+++ NeutroniumApi.Interfaces/Elements/KleiElements/Special.cs
@@ -0,0 +1,17 @@
+﻿using Neutronium.Api.Meta;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Neutronium.Api.Elements.KleiElements
+{
+	[PreviewApi]
+	public static class Special
+	{
+		[PreviewApi]
+		public static readonly string Vacuum = "Vacuum";
+
+		[PreviewApi]
+		public static readonly string Void = "Void";
+	}
+}
